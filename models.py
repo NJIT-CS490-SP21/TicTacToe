01ea9edd65703b4683e8de6706bb91b488a538d3
EC2 Default User <ec2-user@ip-172-31-37-102.ec2.internal>
2021-03-17 03:23:42 +0000

    linted models.py

--- models.py
+++ models.py
@@ -1,21 +1,28 @@
+"""
+creating person model
+"""
 from app import db
 # from flask_sqlalchemy import SQLAlchemy
 
 
 class Person(db.Model):
+    """
+    person class
+    """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     score = db.Column(db.Integer())
 
     def __repr__(self):
         return '<Person %r>' % self.username
-        
-    
-    
     @property
     def toFormat(self):
+        """
+        formatting person
+        """
         return{
             'id' : self.id,
             'username' : self.username,
             'score' : self.score
         }
+        
